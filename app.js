0bb7e3e5b7e6700b5654411b3c20e7c40fcea9cb
lmftasende <106562986+lmftasende@users.noreply.github.com>
2022-07-20 18:04:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express();
 
 const path = require('path');
 
+const port = process.env.PORT || 8000;
+
 app.use(express.static('public'));
 
-app.listen(8000, () =>
+app.listen(port, () =>
 console.log("Servidor levantado")
 );
 
